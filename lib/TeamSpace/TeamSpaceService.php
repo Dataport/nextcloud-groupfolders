d499816ba37415b941cba0a3fd9df546ecb45f58
Tomasz Trillò <tomasz.trillo@dataport.de>
2026-09-08 14:38:51 +0200

    fix: updateTeamSpaceQuota
    Signed-off-by: Tomasz Trillò <tomasz.trillo@dataport.de>

--- lib/TeamSpace/TeamSpaceService.php
+++ lib/TeamSpace/TeamSpaceService.php
@@ -260,30 +260,6 @@ class TeamSpaceService {
 	}
 
 	/**
-	 * Update the storage quota of the team space belonging to the given team.
-	 *
-	 * @param string $circleId The circle single id.
-	 * @param int $quota Quota in bytes; zero means unlimited.
-	 * @return TeamFolder The updated folder.
-	 * @throws \RuntimeException if no team space is linked to the team.
-	 */
-	public function updateTeamSpaceQuota(string $circleId, int $quota): TeamFolder {
-		$folderId = $this->folderManager->getFolderIdByTeamCircleId($circleId);
-		if ($folderId === null) {
-			throw new \RuntimeException('No team space linked to this team');
-		}
-
-		$this->folderManager->setFolderQuota($folderId, $quota);
-
-		$folder = $this->folderManager->getFolder($folderId);
-		if ($folder === null) {
-			throw new \RuntimeException('Team space could not be found after updating quota');
-		}
-
-		return new TeamFolder($folder->id, $folder->mountPoint);
-	}
-
-	/**
 	 * Return all group folders directly accessible to the given team.
 	 *
 	 * This includes the team's dedicated team space as well as regular group
